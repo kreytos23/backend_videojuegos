1e9bb932fdb154f8db2937e41312834f54f4ad7b
kreytos23 <kreytos.pavi10@gmail.com>
2022-06-05 00:08:23 -0500

    listos endpoints para mysql

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const mysql = require("mysql");
 const mongoose = require("mongoose");
-const gameRoutes = require('./routes/game');
+const gameRoutes = require('./routes/MongoGame');
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
@@ -42,20 +42,134 @@ app.get("/hello", (req, res) => {
     res.send('Hello World in express');
 })
 
-app.get("/getgame", (req, res) =>{
+app.get("/mysql/count/year", (req, res) =>{
     const sql = "select * FROM getCountGamesGroupByYear";
     db.query(sql, (err,result) =>{
         res.send(result);
     })
 })
 
-app.get("/getgame/platform", (req, res) =>{
+
+app.get("/mysql/count/platform", (req, res) =>{
     const sql = "select * FROM getGamesByPlatform";
     db.query(sql, (err,result) =>{
         res.send(result);
     })
 })
 
+app.get("/mysql/count/metascore", (req, res) =>{
+    const sql = "SELECT * FROM getgamesbyscore";
+    db.query(sql, (err,result) =>{
+        res.send(result);
+    })
+})
+
+app.get("/mysql/count/userscore", (req, res) =>{
+    const sql = "SELECT * FROM getgamesbyscoreuser";
+    db.query(sql, (err,result) =>{
+        res.send(result);
+    })
+})
+
+app.get("/mysql/worst", (req, res) =>{
+    const sql = "SELECT * FROM get10worstgamesbymetascore";
+    db.query(sql, (err,result) =>{
+        res.send(result);
+    })
+})
+
+app.get("/mysql/best", (req, res) =>{
+    const sql = "SELECT * FROM get10bestgamesbymetascore";
+    db.query(sql, (err,result) =>{
+        res.send(result);
+    })
+})
+
+app.get("/mysql/best/:platform", (req, res) =>{
+    let { platform } = req.params;
+    platform = getPlatform(platform);
+
+    const sql = `CALL get10bestgamesbymetascoreandplatform('${platform}')`;
+    db.query(sql, (err,result) =>{
+        res.send(result[0]);
+    })
+})
+
+app.get("/mysql/worst/:platform", (req, res) =>{
+    let { platform } = req.params;
+    platform = getPlatform(platform);
+
+    const sql = `CALL get10worstgamesbymetascoreandplatform('${platform}')`;
+    db.query(sql, (err,result) =>{
+        res.send(result[0]);
+    })
+})
+
+app.get("/mysql/best/year/:year", (req, res) =>{
+    let { year } = req.params;
+
+    const sql = `CALL get10bestgamesbymetascoreandyear('${year}')`;
+    db.query(sql, (err,result) =>{
+        res.send(result[0]);
+    })
+})
+
+app.get("/mysql/worst/year/:year", (req, res) =>{
+    let { year } = req.params;
+
+    const sql = `CALL get10worstgamesbymetascoreandyear('${year}')`;
+    db.query(sql, (err,result) =>{
+        res.send(result[0]);
+    })
+})
+
 app.listen(PORT, () => {
     console.log('Corriendo 3001');
 });
+
+function getPlatform(platform) {
+    switch (platform) {
+      case "playstation4":
+        return "PlayStation 4";
+      case "stadia":
+        return "Stadia";
+      case "nintendo64":
+        return "Nintendo 64";
+      case "playstation5":
+        return "PlayStation 5";
+      case "dreamcast":
+        return "Dreamcast";
+      case "wiiu":
+        return "Wii U";
+      case "playstationvita":
+        return "PlayStation Vita";
+      case "playstation":
+        return "PlayStation";
+      case "3ds":
+        return "3DS";
+      case "gameboy":
+        return "Game Boy Advance";
+      case "psp":
+        return "PSP";
+      case "wii":
+        return "Wii";
+      case "gamecube":
+        return "GameCube";
+      case "ds":
+        return "DS";
+      case "xbox":
+        return "Xbox";
+      case "playstation2":
+        return "PlayStation 2";
+      case "xboxone":
+        return "Xbox One";
+      case "playstation3":
+        return "PlayStation 3";
+      case "switch":
+        return "Switch";
+      case "xbox360":
+        return "Xbox 360";
+      case "pc":
+        return "PC";
+    }
+  }
